56576409e99f73d7d2609299ddfde99f57d9d4e7
TahaMgady <taha@taha-2.local>
2016-12-24 15:22:39 +0200

    add graph class and generateChildren fun

--- graph.py
+++ graph.py
@@ -13,8 +13,8 @@ class Vertex:
         self.open          = 0
         self.close         = 0
         self.status        = 'notOpened' 
-
 # end __init__()  
+
 # end class Vertex  
 
 '''
@@ -37,4 +37,59 @@ print ( ver1.children[1].currentNumber )
 
 graph = [ ver1, ver2, ver3 ]
 print ( graph[0].children[1].currentnumber )
+'''
+
+class Graph:
+    '''
+        This class has some graph tools.
+    '''
+    def __init__(self, currentNumber):
+        '''
+            This Initilzes the state space graph with currentNumber.
+        '''
+        # Making a Vertex for the currentNumber 
+        if isinstance(currentNumber, int): # making sure of the type int.
+            self.root = Vertex ( currentNumber )
+            '''
+            # Testing the Graph constructor 
+            print ( root.currentNumber )
+            print ( root.level         )
+            print ( root.status        )
+            '''
+# end __init__()
+
+    def generateChildren ( self, vertex ):
+        '''
+            This funciotn take a vertex
+            and returns its children list.
+        '''
+        # Making the children list of vertex.
+        if isinstance(vertex, Vertex): # making sure of the type Vertex.
+            children = []
+            i = 1
+            while i*i < vertex.currentNumber:
+                children.append ( Vertex (vertex.currentNumber - i*i) )
+                i += 1
+            '''
+            # Testing
+            i = 0
+            while i < len(ls):
+                print ( ls[i].currentNumber )
+                i += 1
+            '''
+            return children
+# end generateChildren()
+
+
+# end class graph
+
+'''
+# Testing the graph class
+graph = Graph (  12  )
+print ( "The Root is: " ,  graph.root.currentNumber)
+ls = graph.generateChildren ( Vertex ( 12 ) )
+i = 0
+while i < len(ls):
+    print ( ls[i].currentNumber )
+    i += 1
 '''
